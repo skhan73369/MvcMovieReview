0e400fdb6c9fc0ff0d544b6f4100490bd8599125
Shehriyar Khan <skhan73369@gmail.com>
2017-11-11 14:54:40 -0500

    Add project files.

---
+++ MvcMovie/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+        public int movieID { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+    }
+}
